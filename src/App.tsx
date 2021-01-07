8da7e8dab423057d52681b2bfa1000ab52df0683
AerospaceXu <aerospacexu@gmail.com>
2021-01-07 12:19:55 +0800

    feat: Add router in project.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  NavLink as Link,
+  Redirect,
+} from 'react-router-dom';
+import styled from 'styled-components';
 
-function App() {
+import Home from './pages/Home';
+import Train from './pages/Train';
+
+const Wrapper = styled.div`
+  width: 100%;
+`;
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Router>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/home">Home</Link>
+            </li>
+            <li>
+              <Link to="train">Train</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route exact path="/home">
+            <Home />
+          </Route>
+          <Route exact path="/train">
+            <Train />
+          </Route>
+          <Route exact path="/">
+            <Redirect to="/home" />
+          </Route>
+        </Switch>
+      </Router>
+    </Wrapper>
   );
-}
+};
 
 export default App;
---
+++ src/pages/Train.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Train: React.FC = () => {
+  return <h1>Train Page Works!!!</h1>
+};
+
+export default Train;
